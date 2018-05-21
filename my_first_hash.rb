0e0e167f97e5c14cbb00a2c466d2e595175554ae
Sierra McCahon <smitche4@mail.umw.edu>
2018-05-21 19:45:37 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,11 +32,9 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
 
 shipping_manifest["muskets"] = 2
-shipping_manifest["muskets"]
 
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest["gun powder"] == 4
-shipping_manifest["gun powder"]
+shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
