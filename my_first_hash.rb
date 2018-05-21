bd2acef87bd54ca38528b5554cd4efff50be2386
Sierra McCahon <smitche4@mail.umw.edu>
2018-05-21 19:56:26 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,8 +30,10 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest["gun powder"] = 4
+  shipping_manifest["gun powder"]
   
   shipping_manifest["muskets"] = 2
+  shipping_manifest["muskets"]
   
   puts shipping_manifest
   
