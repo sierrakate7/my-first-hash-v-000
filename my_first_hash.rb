afc2c1cfafcf565ba8f1003086a2c56c27aa391c
Sierra McCahon <smitche4@mail.umw.edu>
2018-05-21 19:46:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,5 +35,6 @@ shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest["gun powder"] = 4
-puts shipping_manifest
 end
+
+puts shipping_manifest
